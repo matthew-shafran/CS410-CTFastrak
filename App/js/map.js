b1ec5ddd18416c8c253226cc1ba7026fd8446dd1
Matthew Shafran <matthew.shafran@gmailcom>
2015-10-23 12:43:49 -0400

    Map works

--- App/js/map.js
+++ App/js/map.js
@@ -1,1 +1,9 @@
 /* Map Javascript file */
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 41.6750, lng: -72.7872},
+    zoom: 8
+  });
+}
