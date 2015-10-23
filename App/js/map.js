2db30d05efe5d2a85974a158d2ce7fced08e18bf
Matthew Shafran <matthew.shafran@gmailcom>
2015-10-23 14:30:33 -0400

    Updated map features and json data

--- App/js/map.js
+++ App/js/map.js
@@ -1,10 +1,6 @@
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
-
+var map;
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 41.6750, lng: -72.7872},
     zoom: 15
   });
@@ -30,31 +26,94 @@ function initMap() {
     handleLocationError(false, infoWindow, map.getCenter());
   }
   
-	//Create a marker
-   var marker = new google.maps.Marker({
-    position: {lat: 41.70, lng: -72.75 },
-    map: map,
-    title: 'Hello World!'
-  });
   
-   var contentString = 'This is a test tooltip';
-   
-   var infowindow = new google.maps.InfoWindow({
-    content: contentString,
-    maxWidth: 200
-  });
+  //Testing create functions
+  createBus(41.6, -72.7872, 1);
+  createBusStop(41.7, -72.7872, 2);
   
-  //var json = 
-  $.getJSON('http://65.213.12.244/realtimefeed/externalfeed/trapezerealtimefeed.json', function(dataset){
-  	console.log(dataset);
-  });
-  
+  $.ajax({
+  	type: "GET",
+  	url: "http://gisdata.hartford.gov/datasets/453fb4c1dff74efdbdb46fadfd257e28_0.geojson",
+  	success: function(dataset){
+  		console.log(dataset);
+  		for(var i = 0; i < dataset.features.length; i++){
+  			var latitude = dataset.features[i].properties.latitude;
+  			var longitude = dataset.features[i].properties.longitude;
+  			var id = dataset.features[i].properties.id;
+  			createBus(latitude, longitude, id);
+  		}
+ 		 },
+ 	error:function(){
+ 		console.log("ERROR");
+ 	}
+ });  
   
 }
 
+//If the browser doesn't support GeoLocation, output an error.
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
+	infoWindow.setPosition(pos);
+  	infoWindow.setContent(browserHasGeolocation ?
                         'Error: The Geolocation service failed.' :
                         'Error: Your browser doesn\'t support geolocation.');
+}
+
+//Creates a bus marker and its' tooltip.
+function createBus(latitude, longitude, busNumber){
+	var marker = new google.maps.Marker({
+    	position: {lat: latitude, lng: longitude },
+    	map: map,
+    	title: 'Bus #' + busNumber
+  	});
+  	var contentString = 'This is a test bus tooltip';
+   
+   	var infoWindow = new google.maps.InfoWindow({
+    	content: contentString,
+    	maxWidth: 200
+  	});
+  
+  	marker.addListener('click', function() {
+  		infoWindow.open(map,marker);
+  	});
+}
+
+//Creates a bus stop marker and its' tooltip.
+function createBusStop(latitude, longitude, busStopNumber){
+	var marker = new google.maps.Marker({
+    	position: {lat: latitude, lng: longitude },
+    	map: map,
+    	title: 'Bus Stop #' + busStopNumber
+  	});
+  	
+  	var contentString = 'This is a test bus stop tooltip';
+   
+   	var infoWindow = new google.maps.InfoWindow({
+    	content: contentString,
+    	maxWidth: 200
+  	});
+  
+  	marker.addListener('click', function() {
+  		infoWindow.open(map,marker);
+  	});
+}
+
+//Not done yet
+function centerMap(){
+	if (navigator.geolocation) {
+    	navigator.geolocation.getCurrentPosition(function(position) {
+    	var pos = {
+    		lat: position.coords.latitude,
+        	lng: position.coords.longitude
+      	};
+
+      	infoWindow.setPosition(pos);
+      	infoWindow.setContent('Location found.');
+      	map.setCenter(pos);
+    	}, function() {
+      	handleLocationError(true, infoWindow, map.getCenter());
+    	});
+  	} else {
+    	// Browser doesn't support Geolocation
+    	handleLocationError(false, infoWindow, map.getCenter());
+  	}
 }
