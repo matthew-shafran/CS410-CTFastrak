2557189ed79df4f416ca50202b0daeb014f9c363
Robert Rotaru <rrotaru@wesleyan.edu>
2015-11-19 15:27:43 -0500

    Iteration 2 report and working routing

--- App/js/map.js
+++ App/js/map.js
@@ -77,42 +77,48 @@ var GTFSInterface = {
  *
  */
 var MapInterface = {
+    origin : { 
+        element : $('#origin-input')
+    },
+    destination : { 
+        element : $('#destination-input')
+    },
     buses : {},
     busterminals : {},
     trips: {},
     markers: [],
     updateMap : function() {
-        (function(_mapInterface) {
-            JSONInterface.getData(function(data) {
-                for (var i = 0; i < data.length; i++) {
-                    // Bus Object
-                    if (data[i].vehicle !== null) {
-                        var bus = data[i];
-                        if (_mapInterface.buses.hasOwnProperty(bus.id)) {
-                            _mapInterface.updateBus(bus);
-                        } else {
-                            _mapInterface.createBus(bus);
-                        }
-                    } 
-                    // Trip/Route Object
-                    else if (data[i].trip_update !== null) {
-                        var trip = data[i];
-                        if (_mapInterface.trips.hasOwnProperty(trip.id)) {
-                            _mapInterface.updateTrip(trip);
-                        } else {
-                            _mapInterface.createTrip(trip);
-                        }
+        JSONInterface.getData(function(data) {
+            for (var i = 0; i < data.length; i++) {
+                // Bus Object
+                if (data[i].vehicle !== null) {
+                    var bus = data[i];
+                    if (MapInterface.buses.hasOwnProperty(bus.id)) {
+                        MapInterface.updateBus(bus);
+                    } else {
+                        MapInterface.createBus(bus);
+                    }
+                } 
+                // Trip/Route Object
+                else if (data[i].trip_update !== null) {
+                    var trip = data[i];
+                    if (MapInterface.trips.hasOwnProperty(trip.id)) {
+                        MapInterface.updateTrip(trip);
+                    } else {
+                        MapInterface.createTrip(trip);
                     }
                 }
-            });
-        })(this);
+            }
+        });
     },
     createMarker : function(data) {
         var marker = new google.maps.Marker({
             position: data.position,
             map: this.map,
             icon: data.icon,
-            title: data.title 
+            title: data.title,
+            animation: data.animation,
+            draggable: data.draggable
         });
        
         marker.infoWindow = new google.maps.InfoWindow({
@@ -120,14 +126,13 @@ var MapInterface = {
             pixelOffset: data.iconOffset,
             maxWidth: 200
         });
-      
-        (function(_mapInterface) {
+        if (!data.noevents) { 
             marker.addListener('mouseover', function() {
-                _mapInterface.closeMarkers();
-                marker.infoWindow.open(_mapInterface.map, marker);
+                MapInterface.closeMarkers();
+                marker.infoWindow.open(MapInterface.map, marker);
             });
             marker.addListener('click', data.callback);
-        })(this);
+        }
 
         this.markers.push(marker);
         return marker;
@@ -139,6 +144,45 @@ var MapInterface = {
     },
     handleLocationError : function(browserHasGeolocation) {
         alert(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
+    },
+    getNearestTerminal : function(routeId) {
+       var dist = Infinity;
+       var nearest_stop_id;
+       for (var i = 0; i < MapInterface.routestops.length; i++) {
+           var stop_id = MapInterface.routestops[i].stop_id;
+           var newdist = distance(MapInterface.busterminals[stop_id].marker.position, MapInterface.traveler.position);
+           if (newdist < dist) {
+               dist = newdist;
+               nearest_stop_id = stop_id;
+           }
+       }
+       return MapInterface.busterminals[nearest_stop_id];
+    },
+    drawDirections : function(destination) {
+        if (MapInterface.route) {
+            MapInterface.directions_renderer.setOptions({polylineOptions:{strokeColor:MapInterface.route.strokeColor}});
+            MapInterface.route.setMap(null);
+        }
+
+        var nearestTerminal = MapInterface.getNearestTerminal();
+
+        // Route walking directions to nearest stop
+        MapInterface.directions.route({
+            origin: MapInterface.traveler.position, 
+            destination: nearestTerminal.marker.position,
+            travelMode: google.maps.TravelMode.WALKING
+        }, function (response, status) {
+            MapInterface.directions_renderer_walk.setDirections(response);
+
+            // Route transit directions to destination
+            MapInterface.directions.route({
+                origin: nearestTerminal.marker.position,
+                destination: destination.position,
+                travelMode: google.maps.TravelMode.DRIVING
+            }, function(response, status) {
+                MapInterface.directions_renderer.setDirections(response);
+            });
+        });
     },
     drawRoute : function(route, stops) {
         if (MapInterface.route) MapInterface.route.setMap(null);
@@ -176,37 +220,35 @@ var MapInterface = {
         }
     },
     createBus : function(bus) {
-        (function(_mapInterface) {
-            var busmarker  = {
-                position : {
-                    lat : bus.vehicle.position.latitude,
-                    lng : bus.vehicle.position.longitude
-                },
-                icon : {
-                    url : 'img/largebus.png',
-                    size: new google.maps.Size(560,182),
-                    origin: new google.maps.Point(0,0),
-                    anchor: new google.maps.Point(25,8),
-                    scaledSize: new google.maps.Size(50,16)
-                },
-                iconOffset : new google.maps.Size(-255,0),
-                title : 'Bus ' + bus.id,
-                content : 'Bus ' + bus.id,
-                
-                // Callback function for when clicking on this marker
-                callback : function() {
-                    var color = "#"+GTFSInterface.data.routes[bus.vehicle.trip.route_id].route_color;
-                    var trip_id = bus.vehicle.trip.trip_id;
-                    var shape_id = GTFSInterface.data.trips[trip_id].shape_id;
-                    var shape = GTFSInterface.data.shapes[shape_id];
-                    shape.color = color;
-                    var stops = _mapInterface.trips[trip_id].trip_update.stop_time_update;
-                    _mapInterface.drawRoute(shape, stops); 
-                }
+        var busmarker  = {
+            position : {
+                lat : bus.vehicle.position.latitude,
+                lng : bus.vehicle.position.longitude
+            },
+            icon : {
+                url : 'img/largebus.png',
+                size: new google.maps.Size(560,182),
+                origin: new google.maps.Point(0,0),
+                anchor: new google.maps.Point(25,8),
+                scaledSize: new google.maps.Size(50,16)
+            },
+            iconOffset : new google.maps.Size(-255,0),
+            title : 'Bus ' + bus.id,
+            content : 'Bus ' + bus.id,
+            
+            // Callback function for when clicking on this marker
+            callback : function() {
+                var color = "#"+GTFSInterface.data.routes[bus.vehicle.trip.route_id].route_color;
+                var trip_id = bus.vehicle.trip.trip_id;
+                var shape_id = GTFSInterface.data.trips[trip_id].shape_id;
+                var shape = GTFSInterface.data.shapes[shape_id];
+                shape.color = color;
+                var stops = MapInterface.trips[trip_id].trip_update.stop_time_update;
+                MapInterface.drawRoute(shape, stops); 
             }
-            bus.marker = _mapInterface.createMarker(busmarker);
-            _mapInterface.buses[bus.id] = bus;
-        })(this);
+        }
+        bus.marker = MapInterface.createMarker(busmarker);
+        MapInterface.buses[bus.id] = bus;
     },
     updateBus : function(bus) {
         var position = new google.maps.LatLng(bus.vehicle.position.latitude, bus.vehicle.position.longitude);
@@ -221,74 +263,67 @@ var MapInterface = {
         this.trips[trip.id] = trip;
     },
     createBusTerminal : function(bus_terminal) {
-        (function(_mapInterface) {
-            var bus_terminal_marker  = {
-                position : {
-                    lat : parseFloat(bus_terminal.stop_lat),
-                    lng : parseFloat(bus_terminal.stop_lon)
-                },
-                icon : {
-                    path: google.maps.SymbolPath.CIRCLE,
-                    scale: 4,
-                    fillColor: '#000000',
-                    fillOpacity: 1.0,
-                    strokeWeight: 1,
-                    strokeOpacity: 0.25
-                },
-                iconOffset : new google.maps.Size(0,0),
-                title : bus_terminal.stop_name + " (#" + bus_terminal.stop_id + ")",
-                content : bus_terminal.stop_name + " (#" + bus_terminal.stop_id + ")",
-                // Sets the destination location
-                callback : function() {
-                    var coords = {lat: this.position.lat(), lng: this.position.lng()};
-                    _mapInterface.geocoder.geocode({'location':coords}, function(results, status) {
-                        $('#destination-input').val(results[0].formatted_address);
-                    });
-                }
+        var bus_terminal_marker  = {
+            position : {
+                lat : parseFloat(bus_terminal.stop_lat),
+                lng : parseFloat(bus_terminal.stop_lon)
+            },
+            icon : {
+                path: google.maps.SymbolPath.CIRCLE,
+                scale: 4,
+                fillColor: '#000000',
+                fillOpacity: 1.0,
+                strokeWeight: 1,
+                strokeOpacity: 0.25
+            },
+            iconOffset : new google.maps.Size(0,0),
+            title : bus_terminal.stop_name + " (#" + bus_terminal.stop_id + ")",
+            content : bus_terminal.stop_name + " (#" + bus_terminal.stop_id + ")",
+            // Sets the destination location
+            callback : function() {
+                MapInterface.drawDirections(this);
             }
-            bus_terminal.marker = _mapInterface.createMarker(bus_terminal_marker);
-            bus_terminal.marker.setVisible(false);
-            _mapInterface.busterminals[bus_terminal.stop_id] = bus_terminal;
-        })(this);
+        }
+        bus_terminal.marker = MapInterface.createMarker(bus_terminal_marker);
+        bus_terminal.marker.setVisible(false);
+
+        MapInterface.busterminals[bus_terminal.stop_id] = bus_terminal;
     },
     updateBusTerminal : function() {
 
     },
     centerMap : function() {
         if (navigator.geolocation) {
-            (function(_mapInterface){
-                navigator.geolocation.getCurrentPosition(function(position) {
-                    var position = {
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude
-                    }
-                    
-                    _mapInterface.geocoder.geocode({'location':position}, function(results, status) {
-                        $('#origin-input').val(results[0].formatted_address);
-                    });
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var position = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                }
 
-                    var traveler_marker  = {
-                        position : position,
-                        icon : {
-                            url: 'img/traveler.png',
-                            size: new google.maps.Size(36,63),
-                            origin: new google.maps.Point(0,0),
-                            anchor: new google.maps.Point(5,25),
-                            scaledSize: new google.maps.Size(10,18)
-                        },
-                        iconOffset : new google.maps.Size(-13,0),
-                        title : 'You are here',
-                        content : 'You are here',
-                    }
+                var traveler_marker  = {
+                    position : position,
+                    draggable: true,
+                    noevents: true,
+                    animation: google.maps.Animation.DROP,
+                    icon : {
+                        url: 'img/traveler.png',
+                        size: new google.maps.Size(36,63),
+                        origin: new google.maps.Point(0,0),
+                        anchor: new google.maps.Point(5,25),
+                        scaledSize: new google.maps.Size(10,18)
+                    },
+                    iconOffset : new google.maps.Size(-13,0),
+                    title : 'You are here',
+                    content : 'You are here',
+                }
 
-                    _mapInterface.createMarker(traveler_marker);
-
-                    _mapInterface.map.setCenter(position);
-                }, 
-                function() {
-                    _mapInterface.handleLocationError(true);
-                });
-            })(this);
+                MapInterface.traveler = MapInterface.createMarker(traveler_marker);
+                MapInterface.traveler.position = new google.maps.LatLng(position.lat, position.lng);
+                MapInterface.map.setCenter(MapInterface.traveler.position);
+            }, 
+            function() {
+                MapInterface.handleLocationError(true);
+            });
         } else {
             this.handleLocationError(false);
         }
@@ -300,19 +335,22 @@ var MapInterface = {
             zoom: 15
         });
         this.geocoder = new google.maps.Geocoder;
-            
+        this.places = new google.maps.places.PlacesService(this.map);
+        this.directions = new google.maps.DirectionsService;
+        this.directions_renderer = new google.maps.DirectionsRenderer;
+        this.directions_renderer_walk = new google.maps.DirectionsRenderer({suppressMarkers:true});
+        this.directions_renderer.setMap(this.map);
+        this.directions_renderer_walk.setMap(this.map);
 
-        (function(_mapInterface) {
-            _mapInterface.map.addListener('click', function() { _mapInterface.closeMarkers(); });
-            _mapInterface.updateInterval = setInterval(function() { _mapInterface.updateMap(); }, 30000);
+        MapInterface.map.addListener('click', function() { MapInterface.closeMarkers(); });
+        MapInterface.updateInterval = setInterval(function() { MapInterface.updateMap(); }, 30000);
 
-            // Load the GTFS static data
-            GTFSInterface.getData(function(data) { 
-                for (var stop_id in data.stops) {
-                    _mapInterface.createBusTerminal(data.stops[stop_id]);
-                }
-            });
-        })(this);
+        // Load the GTFS static data
+        GTFSInterface.getData(function(data) { 
+            for (var stop_id in data.stops) {
+                MapInterface.createBusTerminal(data.stops[stop_id]);
+            }
+        });
 
         // Try HTML5 geolocation.
         this.centerMap();    
@@ -320,29 +358,11 @@ var MapInterface = {
     }
 };
 
-
-function testShapes() {
-    d3.csv("googleha_transit/shapes.txt", function(data) { 
-        var sequenceNum = 0;
-        var shapeCoordinates = [];
-        for (var i = 0; i < data.length; i++) {
-            if (i != 0 && data[i].shape_id !== data[i-1].shape_id) {
-                var shapePath = new google.maps.Polyline({
-                    path: shapeCoordinates,
-                    geodesic: true,
-                    strokeColor: '#FF0000',
-                    strokeOpacity: 1.0,
-                    strokeWeight: 2
-                });
-                shapePath.setMap(MapInterface.map);
-                shapeCoordinates = []; 
-                break;
-            }
-            shapeCoordinates.push({
-                lat: parseFloat(data[i].shape_pt_lat),
-                lng: parseFloat(data[i].shape_pt_lon)
-            });
-            sequenceNum = data[i].shape_pt_sequence;
-        }
-    });
+function distance(coords1, coords2) {
+  		var p = 0.017453292519943295;    // Math.PI / 180
+  		var a = 0.5 - Math.cos((coords2.lat() - coords1.lat()) * p)/2 + 
+          Math.cos(coords1.lat() * p) * Math.cos(coords2.lat() * p) * 
+          (1 - Math.cos((coords2.lng() - coords1.lng()) * p))/2;
+  		return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
 }
+
